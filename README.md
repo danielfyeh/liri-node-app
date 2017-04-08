33adf3ff276e0fad0b112cb1260557efb4006607
Daniel Yeh <Daniel Yeh>
2017-04-08 09:28:24 -0700

    first

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # liri-node-app
+
+LIRI is Language Interpretation & Recognition Interface. LIRI will be a command line node app that takes in parameters and returns data.
+
+
+Description on how to use the app
+
+node functions 
+
+"node liri.js my-tweets" to see last 20 tweets posted
+"node liri.js spotify-this-song 'song name here'" to find more information about the song
+"node liri.js movie-this` " to find more information about the movie
+"node liri.js do-what-it-says" to read the random.txt file and call on one of LIRI's commands
+Requirements
+
+
+Technologies Used
+
+node.js
+npm
+api's (twitter, spotify, omdb)
+
+Code Explaination
+
+Take in a user input in node to use the aformentioned functions.
---
+++ keys.js
@@ -0,0 +1,31 @@
+var Twitter = require('twitter');
+ 
+var client = new Twitter({
+  consumer_key: '',
+  consumer_secret: '',
+  access_token_key: '',
+  access_token_secret: ''
+});
+ 
+var params = {screen_name: 'nodejs'};
+client.get('statuses/user_timeline', params, function(error, tweets, response) {
+  if (!error) {
+    console.log(tweets);
+  }
+});
+
+var request = require('request');
+request('http://www.google.com', function (error, response, body) {
+  console.log('error:', error); // Print the error if one occurred 
+  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received 
+  console.log('body:', body); // Print the HTML for the Google homepage. 
+});
+
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: '<input here>',
+  consumer_secret: '<input here>',
+  access_token_key: '<input here>',
+  access_token_secret: '<input here>',
+}
